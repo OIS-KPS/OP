b08cbcb55a9a9f1758280746a938bd0889ec4ecd
KateC08 <coming.katelyn08@gmail.com>
2026-08-09 12:39:26 +0800

    Updated the path for the styles

--- src/pages/student/profilePage.php
+++ src/pages/student/profilePage.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Profile - ICS OJT Portal</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="public/assets/css/styles.css">
+    <link rel="stylesheet" href="public/css/styles.css">
 </head>
 <body class="bg-slate-50 text-slate-800 antialiased">
 
